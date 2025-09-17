690b813043efe7e223b587180ba23308d600c242
GIZZN <savkinartem50@gmail.com>
2025-09-17 18:35:37 +0300

    final

--- src/app/profile/components/ProfileHeader.tsx
+++ src/app/profile/components/ProfileHeader.tsx
@@ -31,11 +31,6 @@ const ProfileHeader = memo(function ProfileHeader({
           <div className="profile-avatar">
             {profileData.name?.charAt(0).toUpperCase()}
           </div>
-          <button className="avatar-edit-btn">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" stroke="currentColor" strokeWidth="2" fill="none"/>
-            </svg>
-          </button>
         </div>
         
         <div className="profile-info">
